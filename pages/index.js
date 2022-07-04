95fb554cd623042c849315c9a4666d837a752374
Sherry <hhlsdkm@gmail.com>
2022-07-04 18:26:14 +0900

    news page

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+const HomePage = (props) => {
+  return <h1>The Home Page</h1>;
+};
+
+export default HomePage;
---
+++ pages/news.js
@@ -0,0 +1,5 @@
+const NewsPage = (props) => {
+  return <h1>The News Page</h1>;
+};
+
+export default NewsPage;
