7b0b0567c5a978ac0ab0a2273996ebc63397d60c
Sherry <hhlsdkm@gmail.com>
2022-07-11 10:48:16 +0900

    getServerSideProps

--- pages/index.js
+++ pages/index.js
@@ -20,17 +20,30 @@ const DUMMY_MEETUPS = [
 ];
 
 const HomePage = (props) => {
+  console.log(props);
   return <MeetupList meetups={props.meetups} />;
 };
+
+// export const getServerSideProps = async (context) => {
+//   const req = context.req;
+//   const res = context.res;
+
+//   // fetch data from an API
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS,
+//     },
+//   };
+// };
 
 export const getStaticProps = async () => {
   // fetch data from an API
   return {
-    props:{
-      meetups : DUMMY_MEETUPS
-    }
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
+    revalidate: 10,
   };
-
-}
+};
 
 export default HomePage;
